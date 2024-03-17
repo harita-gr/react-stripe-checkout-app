eef3b0a3da56a4316494017883387a5bf547e733
Harita Ravindranath <haritagr19@gmail.com>
2024-03-18 00:03:49 +0530

    be code

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const uuid = require("uuid");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("It works");
+});
+
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log("Product", product);
+  console.log("Price", product.price);
+  const idempotencyKey = uuid(); // To keep track of the payment to ensure same payment is not made twice
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `Purchase of ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              line1: token.card.address_line1,
+              city: token.card.address_city,
+              country: token.card.address_country,
+              postal_code: token.card.address_zip,
+            },
+          },
+        },
+        { idempotencyKey }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
+});
+
+//listen
+app.listen(8282, () => console.log("Server is running on 8282"));
